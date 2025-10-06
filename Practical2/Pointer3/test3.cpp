3af05858dc44a4c8e2d72c91b5d2ece23e5498db
han-re <rehanananth@gmail.com>
2025-10-06 14:06:05 +0100

    Ex 3 finito

--- Practical2/Pointer3/test3.cpp
+++ Practical2/Pointer3/test3.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 void mul_div(double& first, double& second) {
@@ -8,12 +9,35 @@ void mul_div(double& first, double& second) {
 	first = temp;
 }
 
-double a = 10;
-double b = 20;
+double a = 10, b = 20;
 
+void mul_div2(double* first, double* second) {
 
-int main() {
+	double temp = *first * *second;
+	*second = *first / *second;
+	*first = temp;
+	cout << "First: " << *first << " Second: " << *second;
+}
+
+/*int mainzero() {
 	mul_div(a, b);
 	cout << "First: " << a << " Second: " << b;
 	return 0;
-}
+} */
+
+int main() {
+	srand(time(0));
+	double c = (rand() / double(RAND_MAX)) * 5;
+	double d = (rand() / double(RAND_MAX)) * 5;
+	//double* ptr1 = &c;
+	//double* ptr2 = &d;
+
+	double e = 5.0;
+	double f = 10.0;
+	double* ptr1 = &e;
+	double* ptr2 = &f;
+
+	mul_div2(ptr1, ptr2);
+	return 0;
+
+} 
