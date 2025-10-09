294b33f5f2e8a4eef8641bffabf0d0d162961eca
han-re <rehanananth@gmail.com>
2025-10-09 20:42:10 +0100

    Ex 5 + 6 finito

--- Practical2/Arrays/test5.cpp
+++ Practical2/Arrays/test5.cpp
@@ -20,6 +20,27 @@ void printArray(int* data, int size) {
 	}
 }
 
+void printCharArray(char* data, int size) {
+	if (data != NULL && size > 0) {
+		cout << "[";
+		for (int i = 0; i < size; i++) {
+			cout << data[i];
+
+			if (i < size - 1) {
+				cout << ", ";
+			}
+		}
+		cout << "]" << "\n";
+
+	}
+	else {
+		cout << "Error - Invalid Array" << "\n";
+	}
+}
+
+
+
+
 bool bResizeArray(int*& pi, int& size, int sizeNew)
 {
 	// allocate new array
@@ -48,39 +69,94 @@ bool bResizeArray(int*& pi, int& size, int sizeNew)
 }
 
 bool bAddArrays(char*& pchs, int& size, char* pchs2, int size2) {
-	pchs2 = new int[size 2];
-	if (size == size2 || size2 <= 0) {
+	if (size2 <= 0) {
 		return false;
 	}
 
-	if (pchs2 == NULL) {
-		return false;
+
+	else {
+		int temp;
+		temp = size + size2;
+
+		char* temp_array = new char[temp];
+		if (temp_array == NULL) {
+			return false;
+		}
+
+		for (int i = 0; i < size; i++) {
+			temp_array[i] = pchs[i];
+		}
+		for (int i = 0; i < size2; i++) {
+			temp_array[size + i] = pchs2[i];
+		}
+
+		delete[] pchs;
+		pchs = temp_array;
+		size = temp;
+
+		return true;
 	}
 
 
 }
 
+//int main1()
+//{
+//	// initial array
+//	int size = 10;
+//	int* pi = new int[size];
+//	for (int i = 0; i < size; i++)
+//		pi[i] = i;
+//	printArray(pi, size);
+//	cout << endl;
+//	// expanding
+//	if (bResizeArray(pi, size, 20)) {
+//		printArray(pi, size);
+//		cout << endl;
+//	}
+//	// shrinking
+//	if (bResizeArray(pi, size, 5)) {
+//		printArray(pi, size);
+//		cout << endl;
+//	}
+//	// free array memory
+//	delete[] pi;
+//	//return 0;
+//}
 
-int main()
-{
-	// initial array
-	int size = 10;
-	int* pi = new int[size];
-	for (int i = 0; i < size; i++)
-		pi[i] = i;
-	printArray(pi, size);
-	cout << endl;
-	// expanding
-	if (bResizeArray(pi, size, 20)) {
-		printArray(pi, size);
-		cout << endl;
+int main() {
+	//pchs, size, pchs2, size 2
+	srand(time(0));
+
+	int size = rand() % 8 + 3, size2 = rand() % 8 + 3;
+
+	char* pchs = new char[size];
+	char* pchs2 = new char[size2];
+
+
+	
+
+	for (int i = 0; i < size; i++) {
+		char randLetter = rand() % 26 + 'A';
+		pchs[i] = randLetter;
+
 	}
-	// shrinking
-	if (bResizeArray(pi, size, 5)) {
-		printArray(pi, size);
-		cout << endl;
+
+	for (int i = 0; i < size2; i++) {
+		char randLetter = rand() % 26 + 'A';
+		pchs2[i] = randLetter;
 	}
-	// free array memory
-	delete[] pi;
+
+	printCharArray(pchs, size);
+	printCharArray(pchs2, size2);
+	if (bAddArrays(pchs, size, pchs2, size2)) {
+		cout << "LET'S GO!" << "\n";
+		printCharArray(pchs, size);
+	}
+	
+
+	
+	cout << "Size 1 after concatenation: " << size << "\nSize2: " << size2 << "\n";
+	//cout << *pchs << *pchs2;
 	return 0;
 }
---
+++ Practical2/Insert/test6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//boolbInsert(int*& pi, int& size, int pos, int val) {
+//
+//}
+
+int main() {
+	int pos = 3;
+	int a[] = { 1, 2, 3, 4, 5 };
+	for (int i = 0; i < pos; i++) {
+		cout << a[i]
+	}
+
+}
